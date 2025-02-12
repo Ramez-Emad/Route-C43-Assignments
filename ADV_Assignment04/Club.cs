c1f4659fe31a880bec37560e3835b0538f437753
Ramez <ramezemad44@gmail.com>
2025-02-12 06:55:07 +0200

    Add Assignment ADV 04

---
+++ ADV_Assignment04/Club.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV_Assignment04
+{
+    class Club
+    {
+        public int ClubID { get; set; }
+        public String ClubName { get; set; }
+
+        List<Employee> Members;
+        public Club()
+        {
+            Members = new List<Employee>();
+        }
+
+        public void AddMember(Employee E)
+        {
+
+            if (E != null)
+            {
+                Members.Add(E);
+                E.EmployeeLayOff += this.RemoveMember;
+            }
+
+        }
+        public void RemoveMember(object sender, EmployeeLayOffEventArgs e)
+        {
+
+
+            if (sender is Employee Emp && Members.Contains(Emp) && e.Cause == LayOffCause.VacationStockLimit)
+            {
+                Console.WriteLine($"{Emp} Has Been Removed From Club {this.ClubName} because of {e.Cause}");
+                Members.Remove(Emp);
+            }
+
+        }
+    }
+
+}
